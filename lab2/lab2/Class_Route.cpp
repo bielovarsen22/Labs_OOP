9c7d96c8ffbdafbbaddbf0c3facff6711fe6a8bd
BielovArsen <BielovArsen@example.com>
2026-03-20 02:16:24 +0200

    lab3_Clas_Route

--- lab2/lab2/Class_Route.cpp
+++ lab2/lab2/Class_Route.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+int Route::count = 0;
+
 Route::Route()
     : Route("Unknown", "Unknown", 0.0) {
 }
 
 Route::Route(string startPoint, string endPoint, double distance)
     : startPoint(startPoint), endPoint(endPoint), distance(distance) {
+    count++;
 }
 
 Route::Route(string startPoint, string endPoint)
@@ -30,4 +33,60 @@ void Route::printInfo() const
     cout << "Route: " << startPoint
         << " -> " << endPoint
         << " | Distance: " << distance << " km" << endl;
+}
+
+Route::Route(const Route& other)
+{
+    startPoint = other.startPoint;
+    endPoint = other.endPoint;
+    distance = other.distance;
+
+    count++;
+}
+
+Route::Route(Route&& other)
+{
+    startPoint = move(other.startPoint);
+    endPoint = move(other.endPoint);
+    distance = other.distance;
+
+    count++;
+}
+
+void Route::setDistance(double distance)
+{
+    this->distance = distance;
+}
+
+void Route::Routes(double distan)
+{
+    distance += distan;
+}
+
+void Route::showCount()
+{
+    cout << "Number of Route objects: " << count << endl;
+}
+
+Route Route::operator+(const Route& other)
+{
+    return Route(
+        startPoint,
+        endPoint,
+        distance + other.distance
+    );
+}
+
+ostream& operator<<(ostream& out, const Route& r)
+{
+    out << r.startPoint << " " << r.endPoint << " "
+        << r.distance;
+    return out;
+}
+
+istream& operator>>(istream& in, Route& r)
+{
+    cout << "Enter startPoint, endPoint, distance : ";
+    in >> r.startPoint >> r.endPoint >>r.distance;
+    return in;
 }
--- lab2/lab2/Class_Route.h
+++ lab2/lab2/Class_Route.h
@@ -11,14 +11,30 @@ private:
     string endPoint;
     double distance;
 
+    static int count;
+
 public:
     Route();
     Route(string startPoint, string endPoint, double distance);
     Route(string startPoint, string endPoint);
+
+    Route(const Route& other);   
+    Route(Route&& other);
+
     ~Route();
 
     void changeDistance(double newDistance);
+    void Routes(double distan);
     void printInfo() const;
+
+    void setDistance(double distance);
+
+    static void showCount();
+
+    Route operator+(const Route& other);
+
+    friend ostream& operator<<(ostream& out, const Route& r);
+    friend istream& operator>>(istream& in, Route& r);
 };
 
 #endif
