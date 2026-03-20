913679a971a773ae07baca067b22803ad54f3f53
BielovArsen <BielovArsen@example.com>
2026-03-20 02:17:33 +0200

    Lab3_Clas_transport

--- lab2/lab2/Class_transport.cpp
+++ lab2/lab2/Class_transport.cpp
@@ -3,24 +3,58 @@
 
 using namespace std;
 
+// static змінна
+int Transport::count = 0;
 
+// конструктори
 Transport::Transport()
-    : Transport("Unknown", "Unknown", 0, 0.0, 0.0) {
+    : Transport("Unknown", "Unknown", 0, 0.0, 0.0)
+{
 }
 
 Transport::Transport(string brand, string model, int year, double mileage, double fuel)
-    : brand(brand), model(model), year(year), mileage(mileage), fuel(fuel) {
+    : brand(brand), model(model), year(year), mileage(mileage), fuel(fuel)
+{
+    count++;
 }
 
 Transport::Transport(string brand, string model)
-    : Transport(brand, model, 2022, 0.0, 50.0) {
+    : Transport(brand, model, 2022, 0.0, 50.0)
+{
 }
 
+// copy constructor
+Transport::Transport(const Transport& other)
+{
+    brand = other.brand;
+    model = other.model;
+    year = other.year;
+    mileage = other.mileage;
+    fuel = other.fuel;
+
+    count++;
+}
+
+// move constructor
+Transport::Transport(Transport&& other)
+{
+    brand = move(other.brand);
+    model = move(other.model);
+    year = other.year;
+    mileage = other.mileage;
+    fuel = other.fuel;
+
+    count++;
+}
+
+// деструктор
 Transport::~Transport()
 {
+    count--;
     cout << "Transport destroyed\n";
 }
 
+// методи
 void Transport::refuel(double amount)
 {
     fuel += amount;
@@ -38,4 +72,56 @@ void Transport::printInfo() const
         << " | Year: " << year
         << " | Mileage: " << mileage
         << " | Fuel: " << fuel << endl;
+}
+
+// this
+void Transport::setMileage(double mileage)
+{
+    this->mileage = mileage;
+}
+
+// static метод
+void Transport::showCount()
+{
+    cout << "Number of Transport objects: " << count << endl;
+}
+
+// оператор +
+Transport Transport::operator+(const Transport& other)
+{
+    return Transport(
+        brand,
+        model,
+        year,
+        mileage + other.mileage,
+        fuel + other.fuel
+    );
+}
+
+// оператор -
+Transport Transport::operator-()
+{
+    return Transport(
+        brand,
+        model,
+        year,
+        mileage,
+        -fuel
+    );
+}
+
+// <<
+ostream& operator<<(ostream& out, const Transport& t)
+{
+    out << t.brand << " " << t.model << " "
+        << t.year << " " << t.mileage << " " << t.fuel;
+    return out;
+}
+
+// >>
+istream& operator>>(istream& in, Transport& t)
+{
+    cout << "Enter brand model year mileage fuel: ";
+    in >> t.brand >> t.model >> t.year >> t.mileage >> t.fuel;
+    return in;
 }
--- lab2/lab2/Class_transport.h
+++ lab2/lab2/Class_transport.h
@@ -2,8 +2,8 @@
 #define CLASS_TRANSPORT_H
 
 #include <string>
+#include <iostream>
 using namespace std;
-
 
 class Transport
 {
@@ -14,14 +14,36 @@ private:
     double mileage;
     double fuel;
 
+    static int count;
+
 public:
     Transport();
     Transport(string brand, string model, int year, double mileage, double fuel);
     Transport(string brand, string model);
+
+    // 🔥 Конструктори
+    Transport(const Transport& other);   // copy
+    Transport(Transport&& other);        // move
+
     ~Transport();
 
     void refuel(double amount);
     void drive(double distance);
     void printInfo() const;
+
+    // this
+    void setMileage(double mileage);
+
+    // static
+    static void showCount();
+
+    // оператори
+    Transport operator+(const Transport& other);
+    Transport operator-();
+
+    // потоки
+    friend ostream& operator<<(ostream& out, const Transport& t);
+    friend istream& operator>>(istream& in, Transport& t);
 };
+
 #endif
